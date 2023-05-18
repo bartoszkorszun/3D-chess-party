0d48f90f79ccdc6809aa40341565d1cb700ec341
Bartosz Korszun <bartoszkorszun@gmail.com>
2023-05-18 11:36:47 +0200

    Adding project with pieces

---
+++ constants.h
@@ -0,0 +1,9 @@
+const float PI = 3.141592653589793f;
+
+float speed_x = 0;
+float speed_y = 0;
+
+float aspectRatio = 1;
+
+float angle_x = 0;
+float angle_y = 0;
---
+++ main_file.cpp
@@ -0,0 +1,141 @@
+#define GLM_FORCE_RADIANS
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <stdlib.h>
+#include <stdio.h>
+#include <shaderprogram.h>
+#include <constants.h>
+
+#include <bishop.h>
+#include <king.h>
+#include <knight.h>
+#include <pawn.h>
+#include <queen.h>
+#include <rook.h>
+#include <field.h>
+
+ShaderProgram* sp;
+
+void error_callback(int error, const char* description) {
+	fputs(description, stderr);
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (action == GLFW_PRESS) {
+		if (key == GLFW_KEY_LEFT) speed_x = -PI / 2;
+		if (key == GLFW_KEY_RIGHT) speed_x = PI / 2;
+		if (key == GLFW_KEY_UP) speed_y = PI / 2;
+		if (key == GLFW_KEY_DOWN) speed_y = -PI / 2;
+	}
+	if (action == GLFW_RELEASE) {
+		if (key == GLFW_KEY_LEFT) speed_x = 0;
+		if (key == GLFW_KEY_RIGHT) speed_x = 0;
+		if (key == GLFW_KEY_UP) speed_y = 0;
+		if (key == GLFW_KEY_DOWN) speed_y = 0;
+	}
+}
+
+void windowResizeCallback(GLFWwindow* window, int width, int height) {
+	if (height == 0) return;
+	aspectRatio = (float)width / (float)height;
+	glViewport(0, 0, width, height);
+}
+
+void initOpenGLProgram(GLFWwindow* window) {
+
+	glClearColor(0, 0, 0, 1);
+	glEnable(GL_DEPTH_TEST);
+	glfwSetWindowSizeCallback(window, windowResizeCallback);
+	glfwSetKeyCallback(window, keyCallback);
+
+	sp = new ShaderProgram("v_simplest.glsl", NULL, "f_simplest.glsl");
+
+	printf("init\n");
+}
+
+void freeOpenGLProgram(GLFWwindow* window) {
+
+	delete sp;
+
+	printf("free");
+}
+
+void drawScene(GLFWwindow* window) {
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glm::mat4 V = glm::lookAt(
+		glm::vec3(0, 0, -5),
+		glm::vec3(0, 0, 0),
+		glm::vec3(0.0f, 1.0f, 0.0f));
+
+	glm::mat4 P = glm::perspective(50.0f * PI / 180.0f, aspectRatio, 0.01f, 50.0f);
+
+	glm::mat4 M = glm::mat4(1.0f);
+	M = glm::rotate(M, angle_y, glm::vec3(1.0f, 0.0f, 0.0f));
+	M = glm::rotate(M, angle_x, glm::vec3(0.0f, 1.0f, 0.0f));
+
+	sp->use();//Aktywacja programu cieniuj¹cego
+	//Przeslij parametry programu cieniuj¹cego do karty graficznej
+	glUniformMatrix4fv(sp->u("P"), 1, false, glm::value_ptr(P));
+	glUniformMatrix4fv(sp->u("V"), 1, false, glm::value_ptr(V));
+	glUniformMatrix4fv(sp->u("M"), 1, false, glm::value_ptr(M));
+
+	glEnableVertexAttribArray(sp->a("vertex"));  //W³¹cz przesy³anie danych do atrybutu vertex
+	glVertexAttribPointer(sp->a("vertex"), 4, GL_FLOAT, false, 0, bishopVerts); //Wska¿ tablicê z danymi dla atrybutu vertex
+
+	glDrawArrays(GL_TRIANGLES, 0, bishopNumVerts); //Narysuj obiekt
+
+	glDisableVertexAttribArray(sp->a("vertex"));  //Wy³¹cz przesy³anie danych do atrybutu vertex
+
+	glfwSwapBuffers(window);
+}
+
+int main(void) {
+
+	GLFWwindow* window;
+
+	glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit()) {
+		fprintf(stderr, "Nie mo¿na zainicjowaæ GLFW.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	window = glfwCreateWindow(500, 500, "OpenGL", NULL, NULL);
+
+	if (!window)
+	{
+		fprintf(stderr, "Nie mo¿na utworzyæ okna.\n");
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Nie mo¿na zainicjowaæ GLEW.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	initOpenGLProgram(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		angle_x += speed_x * glfwGetTime();
+		angle_y += speed_y * glfwGetTime();
+		glfwSetTime(0);
+		drawScene(window);
+		glfwPollEvents();
+	}
+
+	freeOpenGLProgram(window);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
