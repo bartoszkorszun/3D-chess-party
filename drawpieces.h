ac0178f0675db65724618f3fa7a84127fd3bc5df
Bartosz Korszun <bartoszkorszun@gmail.com>
2023-05-21 21:16:02 +0200

    Added

--- drawpieces.h
+++ drawpieces.h
@@ -1,9 +1,13 @@
 #include <glm/glm.hpp>
+#include <string>
+
+using namespace std;
 
 class DrawPieces {
 public:
 	void drawPieces(glm::mat4 M);
 	void movePieces();
+	void readFile(string filename);
 };
 
 extern DrawPieces *dp;
